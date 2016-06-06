dceccaf640b60826a2d0ee6c4ef1f2c8b373310a
Krzysztof Pazdur <pazdurk@gmail.com>
2016-06-06 08:23:53 +0200

    Using windows.h is bad practice. What if you want run this program on Linux??

--- odc-03/petle3.cpp
+++ odc-03/petle3.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <windows.h>
-
+#include <thread>
 using namespace std;
-
 int main() {
 
     for (int i = 15; i >= 0; i--) {
-        Sleep(1000);
+        this_thread::sleep_for(chrono::milliseconds(1000));
         cin.get();
         cout << i << endl;
     }
